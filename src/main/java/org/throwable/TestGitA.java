f515af1bc753303fc43869e6ebee29d768cbbd9e
zhangjinci <15118850470@163.com>
2017-02-27 15:33:41 +0800

    Initial commit

---
+++ src/main/java/org/throwable/TestGitA.java
@@ -0,0 +1,13 @@
+package org.throwable;
+
+/**
+ * @author throwable
+ * @version v1.0
+ * @since 2017/2/27 15:32
+ */
+public class TestGitA {
+
+    public static void main(String[] args){
+        System.out.println("TestGitA");
+    }
+}
