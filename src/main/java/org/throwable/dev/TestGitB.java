8f175c9b9963c13c6fcdddacdc76885556750627
zhangjinci <15118850470@163.com>
2017-02-27 15:36:56 +0800

    zjc:commit dev
    (cherry picked from commit 5aca616)

---
+++ src/main/java/org/throwable/dev/TestGitB.java
@@ -0,0 +1,13 @@
+package org.throwable.dev;
+
+/**
+ * @author throwable
+ * @version v1.0
+ * @since 2017/2/27 15:36
+ */
+public class TestGitB {
+
+    public static void main(String[] args){
+        System.out.println("TestGitB");
+    }
+}
